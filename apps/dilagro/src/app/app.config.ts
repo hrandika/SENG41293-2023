ff52fa43d95e7d6e3ebbb45ea1aa2c15c39d57ae
hrandika <hrandika@hotmail.com>
2023-07-20 19:03:38 +0530

    pwa added an firebase hosting

--- apps/dilagro/src/app/app.config.ts
+++ apps/dilagro/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { NgxsModule } from '@ngxs/store';
 import { appRoutes } from './app.routes';
 import { AppState } from './state/app/app.state';
@@ -14,8 +19,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     importProvidersFrom(
       NgxsModule.forRoot([AppState], {
-        developmentMode: true,
+        developmentMode: isDevMode(),
       })
     ),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
